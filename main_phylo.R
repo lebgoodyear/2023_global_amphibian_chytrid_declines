0a0b63b2bd59ed25414199a7730b38d3a48c5cb3
lebgoodyear <lebgoodyear@protonmail.com>
2024-04-29 16:44:52 +0100

    Add extra brms parameters
    Input number of cores based on number of cores in computer and match this to
    the number of chains for parallelisation. This will speed up running of model.
    Also add adapt_delta argument, which decrease the step size, thereby reducing
    number of divergences and make sampler more robust. Note that it also slows
    down sampler. Also move phylogeny model later in script, after base model.

--- main_phylo.R
+++ main_phylo.R
@@ -6,20 +6,18 @@
 
 # Author: Luke Goodyear (lgoodyear01@qub.ac.uk)
 # Date created: Apr 2023
-# Last edited: Sep 2023
+# Last edited: Apr 2024
 
 
 ################################################################################
-############################# Run set up script ################################
+############################# Set up script ####################################
 
-
-# this script formats the user input variables for the main script
-# by subsetting dataset by specified variables and creating output directories
-print("Running set up script...")
-source(paste0(scripts_path, "analysis_prep.R"))
 
 # stop Rplots.pdf from being automatically produced when run on command line
 pdf(NULL)
+
+# load packages
+library("brms")
 
 
 ################################################################################
@@ -29,20 +27,59 @@ pdf(NULL)
 # prep phylogeny
 datls <- phylo_compare(df, phylo)
 saveRDS(datls, paste0(path_out, varr, "_", varp, "_data.rds"))
+# split traits and phylogeny
 traits <- datls[[1]] # first list object is dataset
-phylo <- datls[[2]] # second list object is phlogeny
+phylo <- datls[[2]] # second list object is phylogeny
 
 
 ################################################################################
 ################################### brms #######################################
 
 
-# load packages
-library("brms")
+############################## Base model ######################################
+
+
+# model with predictor and intercept only
+if (calc_bay_log == 1) {
+  # get priors
+  priors1 <- get_prior(varr ~ 1 + varp, 
+                       data = dat_brms, 
+                       family = bernoulli())
+  # run brms model
+  mod1 <- brm(varr ~ 1 + varp, 
+              data = dat_brms, 
+              family = bernoulli(), 
+              prior = priors1,
+              iter = iter_log,
+              cores = num_cores,
+              chains = 4,
+              # increasing adapt_delta decreases step size to reduce divergences
+              # it results in a more robust sampler but runs slower
+              control = list(adapt_delta = 0.99))
+  # view model
+  summary(mod1)
+  # save model
+  saveRDS(mod1, paste0(path_out, varr, "_", varp, "_fixed_mod.rds"))
+  # generate diagnostic and posterior distribution plots
+  pdf(file=paste0(path_out, "fixed_postdis.pdf"))
+  hist(mod1[["fit"]]@sim[["samples"]][[1]][["b_varp1"]], breaks=50)
+  dev.off()
+  pdf(file=paste0(path_out, "fixed_postdis_plots.pdf"))
+  plot(mod1)
+  dev.off()
+}
+
+
+######################### Model with phylogeny #################################
+
 
 # create variance-covariance matrix
 A <- ape::vcv.phylo(phylo)
 
+# scale to create correlation matrix to improve convergence
+A <- vcv.phylo(phylo,corr=TRUE)
+
+# split traits and phylogeny
 dat_brms <- as.data.frame(cbind(traits$PhyloName, traits[[varr]], traits[[varp]]))
 names(dat_brms) <- c("PhyloName", "varr", "varp")
 
@@ -50,58 +87,39 @@ names(dat_brms) <- c("PhyloName", "varr", "varp")
 # get priors
 if (calc_phylo == 1) {
   # get priors
-  priors1 <- get_prior(varr ~ 1 + varp + (1|gr(PhyloName, cov = A)), 
+  priors2 <- get_prior(varr ~ 1 + varp + (1|gr(PhyloName, cov = A)), 
                       data = dat_brms, 
                       family = bernoulli(),
                       data2 = list(A = A))
   # run brms model
-  mod1 <- brm(varr ~ 1 + varp + (1|gr(PhyloName, cov = A)), 
+  mod2 <- brm(varr ~ 1 + varp + (1|gr(PhyloName, cov = A)), 
             data = dat_brms, 
             family = bernoulli(), 
             data2 = list(A = A),
-            prior = priors1,
-            iter = iter_phylo)
-  # view model
-  summary(mod1)
-  # save model
-  saveRDS(mod1, paste0(path_out, varr, "_", varp, "_phylo_mod.rds"))
-  # generate diagnostic and posterior distribution plots
-  pdf(file=paste0(path_out, "phylo_fixed_postdis.pdf"))
-  hist(mod1[["fit"]]@sim[["samples"]][[1]][["b_varp1"]], breaks=50)
-  dev.off()
-  pdf(file=paste0(path_out, "phylo_fixed_postdis_plots.pdf"))
-  plot(mod1)
-  dev.off()
-}
-
-# model with predictor and intercept only
-if (calc_bay_log == 1) {
-  # get priors
-  priors2 <- get_prior(varr ~ 1 + varp, 
-                       data = dat_brms, 
-                       family = bernoulli())
-  # run brms model
-  mod2 <- brm(varr ~ 1 + varp, 
-              data = dat_brms, 
-              family = bernoulli(), 
-              prior = priors2,
-              iter = iter_log)
+            prior = priors2,
+            iter = iter_phylo,
+            cores = num_cores,
+            chains = 4,
+            # increasing adapt_delta decreases step size to reduce divergences
+            # it results in a more robust sampler but runs slower
+            control = list(adapt_delta = 0.99))
   # view model
   summary(mod2)
   # save model
-  saveRDS(mod2, paste0(path_out, varr, "_", varp, "_fixed_mod.rds"))
+  saveRDS(mod2, paste0(path_out, varr, "_", varp, "_phylo_mod.rds"))
   # generate diagnostic and posterior distribution plots
-  pdf(file=paste0(path_out, "fixed_postdis.pdf"))
+  pdf(file=paste0(path_out, "phylo_fixed_postdis.pdf"))
   hist(mod2[["fit"]]@sim[["samples"]][[1]][["b_varp1"]], breaks=50)
   dev.off()
-  pdf(file=paste0(path_out, "fixed_postdis_plots.pdf"))
+  pdf(file=paste0(path_out, "phylo_fixed_postdis_plots.pdf"))
   plot(mod2)
   dev.off()
 }
 
+
 # read models in for plotting for further analysis
-#mod1 <- readRDS(paste0(path_out, varr, "_", varp, "_phylo_mod.rds"))
-#mod2 <- readRDS(paste0(path_out, varr, "_", varp, "_fixed_mod.rds"))
+#mod1 <- readRDS(paste0(path_out, varr, "_", varp, "_fixed_mod.rds"))
+#mod2 <- readRDS(paste0(path_out, varr, "_", varp, "_phylo_mod.rds"))
 
 
 ################################################################################
@@ -111,11 +129,11 @@ if (calc_bay_log == 1) {
 sink(file=paste0(path_out, "brms_results.txt"))
 print("brms - fixed effect only")
 print("")
-if (calc_bay_log == 1) {print(summary(mod2))}
+if (calc_bay_log == 1) {print(summary(mod1))}
 print("")
 print("brms - phylogeny control")
 print("")
-if (calc_phylo == 1) {print(summary(mod1))}
+if (calc_phylo == 1) {print(summary(mod2))}
 print("")
 sink()
 
