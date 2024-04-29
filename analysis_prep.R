3983620597b369e603cd9050b46ee317f4fb6582
lebgoodyear <lebgoodyear@protonmail.com>
2024-04-29 17:18:06 +0100

    change na check to null check because it is acting on a vector

--- analysis_prep.R
+++ analysis_prep.R
@@ -5,7 +5,7 @@
 
 # Author: Luke Goodyear (lgoodyear01@qub.ac.uk)
 # Date created: Dec 2022
-# Last edited: Sep 2023
+# Last edited: Apr 2024
 
 
 ############################################################################
@@ -26,11 +26,11 @@ df <- df[!is.na(df[[resp]]),]
 
 # if response column is not already a binary column, convert it to one
 if (length(setdiff(df[[resp]], c(0, 1))) != 0) {
-  if (!is.na(to_remover)) {
+  if (!is.null(to_remover)) {
     print("Removing undesginated entries from response...")
     df <- df[!(df[[resp]] %in% to_remover),]
   }
-  if (!is.na(varr_int)) {
+  if (!is.null(varr_int)) {
     print("Setting up new binary column variable for response...")
     df <- make_new_col(df, varr, resp, varr_int)
   }
@@ -38,7 +38,7 @@ if (length(setdiff(df[[resp]], c(0, 1))) != 0) {
 
 # if predictor column is not already a binary column, convert it to one
 if (length(setdiff(df[[pred]], c(0, 1))) != 0) {
-  if (!is.na(to_removep)) {
+  if (!is.null(to_removep)) {
     print("Removing undesginated entries from predictor...")
     df <- df[!(df[[pred]] %in% to_removep),]
   }
