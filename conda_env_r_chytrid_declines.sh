488d72cfd38713634dd93d9d2a252006e82c98cf
lebgoodyear <lebgoodyear@protonmail.com>
2024-04-29 17:16:10 +0100

    Remove R version from conda script
    Specified version of R was casuing memory leakage issues with brms

--- conda_env_r_chytrid_declines.sh
+++ conda_env_r_chytrid_declines.sh
@@ -2,7 +2,7 @@
 
 # Author: Luke Goodyear lgoodyear01@qub.ac.uk
 # Date created: Apr 2022
-# Last edited: Sep 2023
+# Last edited: Apr 2024
 
 # Description:
 # 1) cleans conda environment 
@@ -16,7 +16,7 @@ conda clean -a -y
 
 # create new environment in one go with all packages to prevent dependency conflicts
 echo "Creating new R conda environment..."
-conda create -n r_chytrid_declines -c conda-forge r-base=4.1.0 r-dplyr r-tidyr r-tibble r-reshape2 r-ggplot2 r-ggpubr r-ape r-geiger r-phytools r-mcmcglmm r-brms r-bh r-sf r-rnaturalearth r-rnaturalearthdata -y
+conda create -n r_chytrid_declines -c conda-forge r-base r-dplyr r-tidyr r-tibble r-reshape2 r-ggplot2 r-ggpubr r-ape r-geiger r-phytools r-mcmcglmm r-brms r-bh r-sf r-rnaturalearth r-rnaturalearthdata -y
 
 # remove any unused packages and caches after installations
 conda clean -a -y
